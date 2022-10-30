6b3aae15bf8f52b7fa5100b94d37e1e184669afa
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2022-10-30 23:31:14 +0500

    add: db and functions

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,13 +1,38 @@
-import {Controller, Get} from "@nestjs/common";
+import {Body, Controller, Delete, Get, Param, Post} from "@nestjs/common";
+import {TrackService} from "./track.service";
+import {CreateTrackDto} from "./dto/create-track.dto";
+import {ObjectId} from "mongoose";
+import {CreateCommentDto} from "./dto/create-comment.dto";
+import {Comment} from "./schemas/comment.schema";
 
 
 @Controller('/tracks')
 export class TrackController {
-    create() {}
+    constructor(private trackService: TrackService) {
+    }
+    
+    @Post()
+    create(@Body() dto: CreateTrackDto) {
+        return this.trackService.create(dto)    
+    }
+    
     @Get()
     getAll() {
-        return 'work'
+        return this.trackService.getAll()
     }
-    getOne() {}
-    delete() {}
+    
+    @Get(':id')
+    getOne(@Param('id') id: ObjectId) {
+        return this.trackService.getOne(id)
+    }
+    
+    @Delete(':id')
+    delete(@Param('id') id: ObjectId) {
+        return this.trackService.delete(id)
+    }
+    
+    @Post('/comment')
+    addComment(@Body() dto: CreateCommentDto): Promise<Comment> {
+        return this.trackService.addComment(dto)
+    }
 }
